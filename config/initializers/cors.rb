d096a485a12c104e4983a2841dedd9faeee81b47
Matan Nahmani <matanmatan999new@gmail.com>
2020-12-16 14:26:41 +0200

    rack cors setup going production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins ENV["API_URL"]
 
     resource '*',
       headers: :any,
