81cf00a4217d4e25ec7314435eff18d8afe8fe06
Matan Nahmani <matanmatan999new@gmail.com>
2020-12-16 15:37:33 +0200

    trying to fix api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,11 +8,10 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["API_URL"]
+    origins "#{ENV["API_URL"]}"
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
