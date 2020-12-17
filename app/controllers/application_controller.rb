eebf448e87deee88cfa3bc5baa067d8a8e77553e
Matan Nahmani <matanmatan999new@gmail.com>
2020-12-17 18:22:42 +0200

    removed csrf for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   include ActionController::RequestForgeryProtection
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   before_action :set_csrf_cookie
 
   private
