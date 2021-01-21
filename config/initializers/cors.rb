832e94cda84eb2cf9b0c8a483573326f56226ec1
Matan Nahmani <matanmatan999new@gmail.com>
2021-01-21 17:36:44 +0200

    small change to env

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["API_URL"]
+    origins ENV["API_URL"] || ENV["API_URL2"]
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
